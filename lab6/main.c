dda7cc0bd38f656b41c2ac7c52b28233c523984e
KiritoKing <kiritoclzh@gmail.com>
2022-12-08 17:12:21 +0800

    format the code files

--- lab6/main.c
+++ lab6/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "../common/common.h"
 
-int main() {
-	printf("hello world!");
+#define RED FB_COLOR(255, 0, 0)
+#define ORANGE FB_COLOR(255, 165, 0)
+#define YELLOW FB_COLOR(255, 255, 0)
+#define GREEN FB_COLOR(0, 255, 0)
+#define CYAN FB_COLOR(0, 127, 255)
+#define BLUE FB_COLOR(0, 0, 255)
+#define PURPLE FB_COLOR(139, 0, 255)
+#define WHITE FB_COLOR(255, 255, 255)
+#define BLACK FB_COLOR(0, 0, 0)
+
+int main()
+{
+	printf("hello world!\n");
+
+	fb_init("/dev/fb0");
+	font_init("./font.ttc");
+
 	return 0;
 }
