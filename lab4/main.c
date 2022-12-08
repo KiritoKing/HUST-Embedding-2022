05e4a1b141441511ced8558a90742614cdde990b
KiritoKing <kiritoclzh@gmail.com>
2022-12-08 13:54:30 +0800

    init

---
+++ lab4/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "../common/common.h"
+
+#define COLOR_BACKGROUND	FB_COLOR(0xff,0xff,0xff)
+
+static int touch_fd;
+static void touch_event_cb(int fd)
+{
+	int type,x,y,finger;
+	type = touch_read(fd, &x,&y,&finger);
+	switch(type){
+	case TOUCH_PRESS:
+		printf("TOUCH_PRESS：x=%d,y=%d,finger=%d\n",x,y,finger);
+		break;
+	case TOUCH_MOVE:
+		printf("TOUCH_MOVE：x=%d,y=%d,finger=%d\n",x,y,finger);
+		break;
+	case TOUCH_RELEASE:
+		printf("TOUCH_RELEASE：x=%d,y=%d,finger=%d\n",x,y,finger);
+		break;
+	case TOUCH_ERROR:
+		printf("close touch fd\n");
+		close(fd);
+		task_delete_file(fd);
+		break;
+	default:
+		return;
+	}
+	fb_update();
+	return;
+}
+
+int main(int argc, char *argv[])
+{
+	fb_init("/dev/fb0");
+	fb_draw_rect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,COLOR_BACKGROUND);
+	fb_update();
+
+	//打开多点触摸设备文件, 返回文件fd
+	touch_fd = touch_init("/dev/input/event2");
+	//添加任务, 当touch_fd文件可读时, 会自动调用touch_event_cb函数
+	task_add_file(touch_fd, touch_event_cb);
+	
+	task_loop(); //进入任务循环
+	return 0;
+}
